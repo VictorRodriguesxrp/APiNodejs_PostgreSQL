d21dfaeb3babca2607a23fd38f5da90c94943b49
Victor Rodrigues <victorxrp@gmail.com>
2021-03-06 12:06:00 -0300

    creating product routes delete/update

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -14,3 +14,40 @@ exports.createProduct = async (req, res) => {
 		}
 	});
 };
+
+exports.listAllProducts = async (req, res) => {
+	const products = await db.query("SELECT * FROM products");
+	res.status(200).send(products.rows);
+};
+
+exports.listProductById = async (req, res) => {
+	const productId = parseInt(req.params.id);
+	const product = await db.query("SELECT * FROM products where productid = $1", [productId]);
+
+	res.status(200).send(product.rows);
+};
+
+exports.updateProductById = async(req, res) => {
+	const productId = parseInt(req.params.id);
+	const { product_name, quantity, price } = req.body;
+	await db.query("UPDATE products set product_name = $1, quantity = $2, price = $3 where productid = $4", [product_name, quantity, price, productId]);
+
+	res.status(200).send({
+		message: "Product updated sucessfully",
+		product : {
+			productId,
+			product_name, 
+			quantity, 
+			price, 
+		} 
+	});
+};
+
+exports.deleteProductById = async(req, res) => {
+	const productId = parseInt(req.params.id);
+	await db.query("DELETE FROM products WHERE productid = $1", [productId]);
+
+	res.status(200).send({
+		message: `Product ${productId} deleted sucessfully`
+	});
+};
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,5 +3,8 @@ const router = require("express-promise-router")();
 const productController = require("../controllers/productController");
 
 router.post("/products", productController.createProduct);
-
+router.get("/products/", productController.listAllProducts);
+router.get("/products/:id", productController.listProductById);
+router.put("/products/:id", productController.updateProductById);
+router.delete("/products/:id", productController.deleteProductById);
 module.exports = router;
